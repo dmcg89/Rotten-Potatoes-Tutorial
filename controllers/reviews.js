1bc99d479842dee4379be65e8712a2cc71b8e3e6
Drew McGowan <michael.mcgowan@students.makeschool.com>
2018-09-14 17:54:35 -0700

    Users can now see comments

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,5 @@
 const Review = require('../models/review')
+const Comment = require('../models/comments')
 
 module.exports = function(app) {
 
@@ -28,14 +29,29 @@ module.exports = function(app) {
         })
     })
 
+    // // SHOW
+    // app.get('/reviews/:id', (req, res) => {
+    //     Review.findById(req.params.id).then((review) => {
+    //         res.render('reviews-show', { review: review })
+    //     }).catch((err) => {
+    //         console.log(err.message);
+    //     })
+    // })
+
     // SHOW
     app.get('/reviews/:id', (req, res) => {
-        Review.findById(req.params.id).then((review) => {
-            res.render('reviews-show', { review: review })
+        // find review
+        Review.findById(req.params.id).then(review => {
+            // fetch its comments
+            Comment.find({ reviewId: req.params.id }).then(comments => {
+                // respond with the template with both values
+                res.render('reviews-show', { review: review, comments: comments })
+            })
         }).catch((err) => {
-            console.log(err.message);
-        })
-    })
+            // catch errors
+            console.log(err.message)
+        });
+    });
 
     // EDIT
     app.get('/reviews/:id/edit', function (req, res) {
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
 const reviews = require('./controllers/reviews');
+const comments = require('./controllers/comments');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
@@ -12,6 +13,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 reviews(app);
+comments(app);
 
 app.listen(process.env.PORT || '3000', () => {
     console.log(`App listening on port 3000!`)
